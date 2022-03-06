867a725658faec6433e32e8a78c9131fabfec244
Nicolas Hiroki Yonekawa <69517285+nicolas2602@users.noreply.github.com>
2022-03-06 17:37:09 -0300

    teste

---
+++ teste/teste.php
@@ -0,0 +1,19 @@
+<?php 
+
+echo "<h1>Média de Notas</h1>";
+
+$n1 = 10;
+$n2 = 9;
+$n3 = 6;
+
+$media = ($n1 + $n2 + $n3)/3;
+
+
+echo "Nota 1: ".$n1."<br/>";
+echo "Nota 2: ".$n2."<br/>";
+echo "Nota 3: ".$n3."<br/>";
+echo "<br/>";
+echo "<b>A média é: </b>".$media;
+
+echo "<hr/>";
+?>
